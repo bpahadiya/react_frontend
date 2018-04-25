4b50dfc77da8776d98cd4de635616f8f1bfb266f
sankalp <sjoshi@throughouttechnologies.com>
2018-04-25 16:08:40 +0530

    sign_in form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sign_in from './Sign_in';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sign_in />, document.getElementById('Login'));
+
 registerServiceWorker();
