295d38edf564eb43293854b7ba3ce85af5c48dc9
sankalp <sjoshi@throughouttechnologies.com>
2018-07-20 12:57:22 +0530

    changing name dynamically on typing according to unique ids

--- src/App.js
+++ src/App.js
@@ -8,34 +8,51 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {name: 'a', age: 1},
-      {name: 'b', age: 2},
-      {name: 'c', age: 3}
+      {id: '1', name: 'a', age: 1},
+      {id: '2', name: 'b', age: 2},
+      {id: '3', name: 'c', age: 3}
     ],
     otherstate: 'some other values',
     showPersons: false
   }
 
-  switchName = (test) => {
-    // console.log('clicked');
-    // this.state.persons[0].name = 'guta' -- We cant use this to change the name so dont do this
-    this.setState({
-      persons: [
-        {name: test, age: 4},
-        {name: 'e', age: 5},
-        {name: 'f', age: 6}
-      ] 
-    }) // setState is a method defined in component library. it is responsible for update the DOM.
-  }
+  // switchName = (test) => {
+  //   // console.log('clicked');
+  //   // this.state.persons[0].name = 'guta' -- We cant use this to change the name so dont do this
+  //   this.setState({
+  //     persons: [
+  //       {id: '1', name: test, age: 4},
+  //       {id: '2', name: 'e', age: 5},
+  //       {id: '3', name: 'f', age: 6}
+  //     ] 
+  //   }) // setState is a method defined in component library. it is responsible for update the DOM.
+  // }
 
-  namechange = (event) => {
-    this.setState({
-      persons: [
-        {name: 'd', age: 4},
-        {name: event.target.value, age: 5}, // now e should change its name.
-        {name: 'f', age: 6}
-      ] 
-    })
+  namechange = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    // const person = this state.persons[perosnIndex]; - we dont directly mutate the object.we will use spread operator for that.
+    // const person = Object.assign({}, this.state.persons[personIndex]) - we cal also use this as a alternative.
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+  //   this.setState({
+  //     persons: [
+  //       {id: '4', name: 'd', age: 4},
+  //       {id: '5', name: event.target.value, age: 5}, // now e should change its name.
+  //       {id: '6', name: 'f', age: 6}
+  //     ] 
+  //   })
+  // }
+
+    this.setState({persons: persons});
   }
 
   togglepersons = () => {
@@ -46,7 +63,8 @@ class App extends Component {
   }
 
   deleteperson = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = this.state.persons; //it is good ractice to create a copy of your array which you want to manipulate.
+    // const persons = [...this.state.persons]; // you can also do this. 
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   }
@@ -71,6 +89,8 @@ class App extends Component {
                 age={person.age}
                 // click = {this.deleteperson.bind(index)} /> this will also work
                 click = {() => this.deleteperson(index)}
+                key = {person.id}
+                changed = {(event) => this.namechange(event, person.id)}
               />
             })}
           </div> 
